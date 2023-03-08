27bb3db9abe2d2bf4f373f96aaf571adac9e108f
OrenKotSalomon <orensalomonkot@gmail.com>
2023-03-08 14:12:25 +0200

    added markers to map

--- frontend/src/cmps/map-pricing.tsx
+++ frontend/src/cmps/map-pricing.tsx
@@ -1,18 +1,48 @@
-import React, { useState } from 'react';
+import React from "react";
+import GoogleMapReact from 'google-map-react';
 
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 interface IMapState {
     lat: number;
     lng: number;
-    zoom: number;
+    text: string;
 }
+const AnyReactComponent = ({ text }: IMapState) => <div className="map-marker">{text}</div>;
+
 export function MapPricing() {
-    const center = { lat: 51.505, lng: -0.09 };
-    const zoom = 2;
-    const googleMapsApiKey = 'AIzaSyA8SqR_MWlmUlCNuXIdfpWY3LqRg4LHvjQ'
-    return <>
-        <div>
+    const defaultProps = {
+        center: {
+            lat: 10.99835602,
+            lng: 77.01502627
+        },
+        zoom: 1
+    };
 
-        </div>
+    // Important! Always set the container height explicitly
+    return <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+            bootstrapURLKeys={{ key: 'AIzaSyA8SqR_MWlmUlCNuXIdfpWY3LqRg4LHvjQ' }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+        >
 
-    </>
+            <AnyReactComponent
+                lat={59.955413}
+                lng={30.337844}
+                text="123$"
+            />
+            <AnyReactComponent
+                lat={54.955413}
+                lng={30.337844}
+                text="1123$"
+            />
+            <AnyReactComponent
+                lat={57.955413}
+                lng={30.337844}
+                text="1223$"
+            />
+
+        </GoogleMapReact>
+    </div>
+
 }
